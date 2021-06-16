a1d38aaf29d690dc2b9ae63811a238d18b33f4e2
Bharath Uday <bharath.uday42@gmail.com>
2021-06-16 22:04:42 +0530

    Changes Made: 1)Added code for Graph,Heap and Tree

---
+++ HashTable/main.go
@@ -0,0 +1,92 @@
+package main
+
+
+import (
+	"math"
+
+	"github.com/dorin131/go-data-structures/linkedlist"
+)
+
+const arrayLength = 100
+
+// HashTable : a hash table data structure
+type HashTable struct {
+	data [arrayLength]*linkedlist.LinkedList
+}
+
+type listData struct {
+	key   string
+	value interface{}
+}
+
+// New : a hash table constructor
+func New() *HashTable {
+	return &HashTable{
+		[arrayLength]*linkedlist.LinkedList{},
+	}
+}
+
+func hash(s string) int {
+	h := 0
+	// char[0] * 31^n-1 + char[1] * 31^n-2 + ... + char[n-1]
+	for pos, char := range s {
+		h += int(char) * int(math.Pow(31, float64(len(s)-pos+1)))
+	}
+	return h
+}
+
+func index(hash int) int {
+	return hash % arrayLength
+}
+
+// Set : set a key and value
+func (h *HashTable) Set(k string, v interface{}) *HashTable {
+	index := index(hash(k))
+
+	if h.data[index] == nil {
+		h.data[index] = linkedlist.New()
+		h.data[index].Append(listData{k, v})
+	} else {
+		node := h.data[index].Head
+		for {
+			if node != nil {
+				d := node.Data.(listData)
+				if d.key == k {
+					d.value = v
+					break
+				}
+			} else {
+				h.data[index].Append(listData{k, v})
+				break
+			}
+			node = node.Next
+		}
+	}
+	return h
+}
+
+// Get : get a value by key
+func (h *HashTable) Get(k string) (result interface{}, ok bool) {
+	index := index(hash(k))
+	linkedList := h.data[index]
+
+	if linkedList == nil {
+		return "", false
+	}
+	node := linkedList.Head
+	for {
+		if node != nil {
+			d := node.Data.(listData)
+			if d.key == k {
+				return d.value, true
+			}
+		} else {
+			return "", false
+		}
+		node = node.Next
+	}
+}
+
+func main() {
+
+}
---
+++ Heap/MaxHeap/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"log"
+
+	"github.com/dorin131/go-data-structures/heap"
+)
+
+// MaxHeap : represents a Max heap data structure
+type MaxHeap struct {
+	*heap.Heap
+}
+
+// New : returns a new instance of a Heap
+func New(input []int) *MaxHeap {
+	h := &MaxHeap{
+		&heap.Heap{
+			Items: input,
+		},
+	}
+
+	if len(h.Items) > 0 {
+		h.buildMaxHeap()
+	}
+
+	return h
+}
+
+func (h *MaxHeap) buildMaxHeap() {
+	for i := len(h.Items)/2 - 1; i >= 0; i-- {
+		h.maxHeapifyDown(i)
+	}
+}
+
+// Insert : adds an element to the heap
+func (h *MaxHeap) Insert(item int) *MaxHeap {
+	h.Items = append(h.Items, item)
+	lastElementIndex := len(h.Items) - 1
+	h.maxHeapifyUp(lastElementIndex)
+
+	return h
+}
+
+// ExtractMax : returns the maximum element and removes it from the Heap
+func (h *MaxHeap) ExtractMax() int {
+	if len(h.Items) == 0 {
+		log.Fatal("No items in the heap")
+	}
+	minItem := h.Items[0]
+	lastIndex := len(h.Items) - 1
+	h.Items[0] = h.Items[lastIndex]
+
+	// shrinking slice
+	h.Items = h.Items[:len(h.Items)-1]
+
+	h.maxHeapifyDown(0)
+
+	return minItem
+}
+
+func (h *MaxHeap) maxHeapifyUp(index int) {
+	for h.HasParent(index) && h.Parent(index) < h.Items[index] {
+		h.Swap(h.GetParentIndex(index), index)
+		index = h.GetParentIndex(index)
+	}
+}
+
+func (h *MaxHeap) maxHeapifyDown(index int) {
+	// iterate until we have a child node smaller than the index value
+	for (h.HasLeft(index) && h.Items[index] < h.Left(index)) ||
+		(h.HasRight(index) && h.Items[index] < h.Right(index)) {
+		// if both children are smaller
+		if (h.HasLeft(index) && h.Items[index] < h.Left(index)) &&
+			(h.HasRight(index) && h.Items[index] < h.Right(index)) {
+			// compare the children and swap with the largest
+			if h.Left(index) > h.Right(index) {
+				h.Swap(index, h.GetLeftIndex(index))
+				index = h.GetLeftIndex(index)
+			} else {
+				h.Swap(index, h.GetRightIndex(index))
+				index = h.GetRightIndex(index)
+			}
+			// else if only left child is smaller than swap with it
+		} else if h.HasLeft(index) && h.Items[index] < h.Left(index) {
+			h.Swap(index, h.GetLeftIndex(index))
+			index = h.GetLeftIndex(index)
+			// else it must be the right child that is smaller, so we swap with it
+		} else {
+			h.Swap(index, h.GetRightIndex(index))
+			index = h.GetRightIndex(index)
+		}
+	}
+}
+
+func main() {
+
+}
